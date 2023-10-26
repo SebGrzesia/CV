ddf0ee65b3b6a5d0c410018eb2c6430682772702
TSARBOMBA <dicoud@gmail.com>
2023-10-26 20:35:49 +0200

    Initial create

---
+++ WebApplication1/Models/UserDetail.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class UserDetail : User
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string[] Skills { get; set; }
+        public string[] Language { get; set; }
+        public string About { get; set; }
+        
+        public UserDetail()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
---
+++ WebApplication1/Models/WorkExp.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class WorkExp : User
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+        public string Position { get; set; }
+        public string Details { get; set; }
+
+        public WorkExp()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
